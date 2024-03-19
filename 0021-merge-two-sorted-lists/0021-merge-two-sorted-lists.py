16ea025d2f8eae24dd990e56f4e47f4910023d1c
Eun Gi Ko <121347506+sliverKi@users.noreply.github.com>
2024-03-19 21:30:26 +0900

    Time: 31 ms (91.88%), Space: 16.5 MB (60.22%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -4,20 +4,20 @@
 #         self.val = val
 #         self.next = next
 
-#시간복잡도(time):O(m+n)
-#공간복잡도(space):O(1)
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        temp = ListNode(None)#temp: pointer 초기화 
-        node = temp#node == pointer
+        dummy = ListNode(None)
+        node = dummy
+        
         while list1 and list2:
             if list1.val < list2.val:
                 node.next = list1
                 list1 = list1.next
             else:
                 node.next=list2
-                list2 = list2.next
-            node = node.next
+                list2=list2.next
+            node=node.next
+            
         node.next = list1 or list2
-        return temp.next
+        return dummy.next
         
